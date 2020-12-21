fadbfb58fcc90ac28745fc04a572c95c8f81bbf8
Lehs <perolehs@gmail.com>
2020-12-21 08:59:03 -0400

    add dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  themoviedb: {
+    api_key: "678ba86d3982556f4f41552fadd87b62",
+    version: 3,
+    url_base: "https://api.themoviedb.org",
+    url_img_base: "https://image.tmdb.org/t/p/"
+  }
 };
